8d5e0bc9f09116c4785725b4eb583087414407b4
phuclee <phuc@192.168.1.23.non-exists.ptr.local>
2025-03-02 07:22:13 -0800

    add fish to nix

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -16,11 +16,12 @@
   ];
 
   programs = {
+    zsh = import ../home/zsh.nix {inherit config pkgs;};
+    fish = import ../home/fish.nix {inherit config pkgs;};
+    nushell = import ../home/nushell.nix {inherit config pkgs;};
     bat = import ../home/bat.nix {inherit config pkgs;};
     git = import ../home/git.nix {inherit config pkgs;};
-    zsh = import ../home/zsh.nix {inherit config pkgs;};
     zoxide = import ../home/zoxide.nix {inherit config pkgs;};
-    nushell = import ../home/nushell.nix {inherit config pkgs;};
     atuin = import ../home/atuin.nix {inherit config pkgs;};
     oh-my-posh = import ../home/oh-my-posh.nix {inherit config pkgs;};
     starship = import ../home/starship.nix {inherit config pkgs;};
