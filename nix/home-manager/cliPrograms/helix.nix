6fc4c59f6e26de9a7b7c27f8db84f96bfaa6a014
phuclee <phuc@192.168.1.23.non-exists.ptr.local>
2025-03-04 00:19:40 +0700

    migrate helix

---
+++ nix/home-manager/cliPrograms/helix.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... };
+
+{
+  enable = true;
+  settings = {
+    theme = "catppuccin_mocha";
+    editor = {
+      evil = true;
+      line-number = "relative";
+      cursorline = true;
+      color-modes = true;
+
+      lsp.display-messages = true;
+
+      cursor-shape = {
+        insert = "bar";
+        normal = "block";
+        select = "underline";
+      };
+
+      indent-guides = {
+        render = true;
+      };
+    keys.normal = {
+      space.space = "file_picker";
+      space.w = ":w";
+      space.q = ":q";
+      esc = [ "collapse_selection" "keep_primary_selection" ];
+    };
+  };
+};
