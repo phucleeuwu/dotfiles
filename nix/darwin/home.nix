beed79a817ef22d832859dd0be9a1b197e2244a7
phuclee <phuc@192.168.1.13.non-exists.ptr.local>
2025-03-06 22:37:15 +0700

    can't config lazyvim

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -1,8 +1,28 @@
-{ inputs, ... }:
-{
-  imports = [ inputs.LazyVim.homeManagerModules.default ];
+{ config, pkgs, lib, inputs, ... }:
 
-  programs.lazyvim = {
-    enable = true;
-  };
+{
+  programs.home-manager.enable = true;
+  home.stateVersion = "25.05"; # Don't change unless necessary.
+
+  # home.username = "phuc"; # Ensure this matches your username.
+  # home.homeDirectory = "/Users/phuc"; # Ensure this matches your homeDirectory.
+  xdg.enable = true;
+
+  imports = [
+    ../home-manager/packages.nix    # Packages list
+    ../home-manager/env.nix         # Session variables
+    ../home-manager/default.nix        # Module
+    ../home-manager/aliases.nix       # ShellAliases
+    # inputs.LazyVim.homeManagerModules.default
+  ];
+
+  # enable or disable programs
+  aerospace.enable = true;
+  fish.enable = true;
+  starship.enable = false;
+  ghostty.enable = false;
+
+  # programs.lazyvim = {
+  #   enable = true;
+  # };
 }
