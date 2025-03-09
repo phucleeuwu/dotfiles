8612ff288cb1f30539d4f73e8173f766ad7906a3
phucleeuwu <phucleeuwu@gmail.com>
2025-03-09 18:02:01 +0700

    c

--- nix/darwin/system.nix
+++ nix/darwin/system.nix
@@ -22,46 +22,47 @@
     };
 
     dock = {
-      autohide = true;
-      mru-spaces = false;
-      static-only = true;
       tilesize = 40;
       autohide-delay = 0.0;
       autohide-time-modifier = 0.0;
-      magnification = true;
       largesize = 80;
-      showhidden = true;
+      mru-spaces = false;
       show-recents = false;
+      magnification = true;
+      showhidden = true;
+      autohide = true;
+      static-only = true;
     };
 
     finder = {
       FXPreferredViewStyle = "clmv";
+      FXDefaultSearchScope = "SCcf";
+      NewWindowTarget = "Home";
+      CreateDesktop = false;
       ShowStatusBar = true;
       ShowPathbar = true;
       FXRemoveOldTrashItems = true;
-      FXDefaultSearchScope = "SCcf";
-      CreateDesktop = false;
-      NewWindowTarget = "Home";
     };
 
     NSGlobalDomain = {
-      _HIHideMenuBar = true;
-      AppleShowAllFiles = true;
-      AppleShowAllExtensions = true;
       AppleInterfaceStyle = "Dark";
+      InitialKeyRepeat = 15;
       AppleKeyboardUIMode = 3;
+      KeyRepeat = 2;
       NSAutomaticCapitalizationEnabled = false;
       NSAutomaticDashSubstitutionEnabled = false;
       NSAutomaticPeriodSubstitutionEnabled = false;
       NSAutomaticQuoteSubstitutionEnabled = false;
       NSAutomaticSpellingCorrectionEnabled = false;
-      InitialKeyRepeat = 15;
-      KeyRepeat = 2;
+      _HIHideMenuBar = true;
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
       "com.apple.sound.beep.volume" = 0.6065307;
       "com.apple.trackpad.scaling" = 3.0;
       "com.apple.swipescrolldirection" = false;
     };
   };
+
     # services = {
     #   karabiner-elements.enable = true;
     # };
