9f600a042933c4d6c4440d49b7829f4637d93225
phuclee <phuc@192.168.1.22.non-exists.ptr.local>
2025-03-16 22:31:53 +0700

    now is the best lazyvim config for home manager, full lazyvim distro no extra config

--- nix/home/cli/neovim.nix
+++ nix/home/cli/neovim.nix
@@ -1,40 +1,40 @@
 { pkgs, ... }:
 
 {
-	enable = true;
-    extraPackages = with pkgs; [
-      nil
-      lua-language-server
-      stylua
-    ];
+  enable = true;
+  extraPackages = with pkgs; [
+    nil
+    lua-language-server
+    stylua
+  ];
 
-    plugins = with pkgs.vimPlugins; [
-      lazy-nvim
-    ];
+  plugins = with pkgs.vimPlugins; [
+    lazy-nvim
+  ];
 
-    extraLuaConfig =
-      ''
-      require("lazy").setup({
-        defaults = {
-          lazy = true,
-        },
-        dev = {
-          -- reuse files from pkgs.vimPlugins.*
-          patterns = { "" },
-          -- fallback to download
-          fallback = true,
-        },
-        spec = {
-          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-          -- The following configs are needed for fixing lazyvim on nix
-          -- disable mason.nvim, use config.extraPackages
-          { "williamboman/mason-lspconfig.nvim", enabled = false },
-          { "williamboman/mason.nvim", enabled = false },
-          -- uncomment to import/override with your plugins
-          -- { import = "plugins" },
-          -- put this line at the end of spec to clear ensure_installed
-          { "nvim-treesitter/nvim-treesitter", opts = function(_, opts) opts.ensure_installed = {} end },
-        },
-      })
-      '';
+  extraLuaConfig =
+    ''
+    require("lazy").setup({
+      defaults = {
+        lazy = true,
+      },
+      dev = {
+        -- reuse files from pkgs.vimPlugins.*
+        patterns = { "" },
+        -- fallback to download
+        fallback = true,
+      },
+      spec = {
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        -- The following configs are needed for fixing lazyvim on nix
+        -- disable mason.nvim, use config.extraPackages
+        { "williamboman/mason-lspconfig.nvim", enabled = false },
+        { "williamboman/mason.nvim", enabled = false },
+        -- uncomment to import/override with your plugins
+        -- { import = "plugins" },
+        -- put this line at the end of spec to clear ensure_installed
+        { "nvim-treesitter/nvim-treesitter", opts = function(_, opts) opts.ensure_installed = {} end },
+      },
+    })
+    '';
 }
