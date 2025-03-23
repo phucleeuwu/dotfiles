dc07f8924b06c3ab6943af0cf67442d261eb5f52
phuclee <phuc@192.168.1.16.non-exists.ptr.local>
2025-03-23 20:38:43 +0700

    change all to maple mono nf font

--- nix/home/gui/ghostty.nix
+++ nix/home/gui/ghostty.nix
@@ -5,7 +5,7 @@ _: {
   enableZshIntegration = true;
   installBatSyntax = true;
   settings = {
-    font-family = "Maple Mono";
+    font-family = "Maple Mono NF";
     font-size = 19;
     mouse-hide-while-typing = true;
     macos-option-as-alt = true;
