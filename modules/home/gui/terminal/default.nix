2ed9041453c94930665f450521c1cf5a05192af4
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-05-14 11:02:09 +0700

    dead code

--- modules/home/gui/terminal/default.nix
+++ modules/home/gui/terminal/default.nix
@@ -6,8 +6,8 @@
 }: {
   options.${flake.config.me.namespace}.terminal = {
     wezterm.enable = lib.mkEnableOption "terminal.wezterm";
-    ghostty.enable = lib.mkEnableOption "terminal.ghostty";
     kitty.enable = lib.mkEnableOption "terminal.kitty";
+    ghostty.enable = lib.mkEnableOption "terminal.ghostty";
   };
   imports = [./all.nix];
 }
