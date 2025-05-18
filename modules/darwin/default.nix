b23fd511fb6abed16edd9d3023b08a5877cdf3c5
phucleeuwu <phucleeuwu@gmail.com>
2025-05-18 08:37:21 +0700

    fix the darwin error

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -8,6 +8,7 @@ in {
       users.users.${config.me.username} = {
         home = "/Users/${config.me.username}";
       };
+      system.primaryUser = "${config.me.username}";
       home-manager = {
         users.${config.me.username} = {};
         sharedModules = [
