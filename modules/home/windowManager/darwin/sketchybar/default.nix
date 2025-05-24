c34cacb558e83bf66ac3af9eac866677ac45b87d
phucleeuwu <phucleeuwu@gmail.com>
2025-05-24 10:24:20 +0700

    clean up the sketchybar config

--- modules/home/windowManager/darwin/sketchybar/default.nix
+++ modules/home/windowManager/darwin/sketchybar/default.nix
@@ -12,12 +12,12 @@
     source = "${pkgs.sketchybar-app-font}/lib/sketchybar-app-font/icon_map.lua";
   };
   xdg.configFile."sketchybar/sketchybarrc" = {
+    executable = true;
     text = ''
       #!/usr/bin/env ${lib.getExe pkgs.lua54Packages.lua}
       package.cpath = package.cpath .. ";${pkgs.lua54Packages.getLuaCPath pkgs.sbarlua}"
       require("helpers")
       require("init")
     '';
-    executable = true;
   };
 }
