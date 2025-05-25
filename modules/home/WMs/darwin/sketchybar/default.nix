cb1f46a66a0d41e98ebd8280c128bd0557f48fd4
phucleeuwu <phucleeuwu@gmail.com>
2025-05-25 22:53:18 +0700

    format it again again

--- modules/home/WMs/darwin/sketchybar/default.nix
+++ modules/home/WMs/darwin/sketchybar/default.nix
@@ -4,15 +4,16 @@
   ...
 }: {
   home.packages = [pkgs.sketchybar-app-font];
-  xdg.configFile."sketchybar" = {
+  xdg.configFile = {
+    "sketchybar" = {
     source = ./.;
     recursive = true;
     onChange = "${lib.getExe pkgs.sketchybar} --reload";
   };
-  xdg.configFile."sketchybar/helpers/app_icons.lua" = {
+  "sketchybar/helpers/app_icons.lua" = {
     source = "${pkgs.sketchybar-app-font}/lib/sketchybar-app-font/icon_map.lua";
   };
-  xdg.configFile."sketchybar/sketchybarrc" = {
+  "sketchybar/sketchybarrc" = {
     executable = true;
     text = ''
       #!/usr/bin/env ${lib.getExe pkgs.lua54Packages.lua}
@@ -21,4 +22,5 @@
       require("init")
     '';
   };
-}
+};
+};
