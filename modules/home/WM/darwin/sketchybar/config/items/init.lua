370e2f315ce00bd5a85fe3dab9c8219e828e75fa
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-06-08 11:03:41 +0700

    remove annoying items

--- modules/home/WM/darwin/sketchybar/config/items/init.lua
+++ modules/home/WM/darwin/sketchybar/config/items/init.lua
@@ -3,9 +3,6 @@ require("items.apple")
 require("items.spaces")
 require("items.menus")
 
---center
-require("items.notifications")
-
 --right (reverse order)
 require("items.calendar")
 require("items.widgets")
