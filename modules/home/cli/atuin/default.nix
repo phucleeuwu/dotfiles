f93fba34f54a090b6acd1c2613a6efdce0623a85
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-06-11 00:57:29 +0700

    clean atuin

--- modules/home/cli/atuin/default.nix
+++ modules/home/cli/atuin/default.nix
@@ -9,10 +9,7 @@
     programs.atuin = {
       enable = true;
       settings = {
-        auto_sync = true;
         records = true;
-        search_mode = "fuzzy";
-        style = "compact";
         inline_height = 20;
         enter_accept = true;
         keymap_mode = "vim-insert";
