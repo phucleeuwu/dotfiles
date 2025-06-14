699b1b8d9da7ba65a1516ffd417457e593bfb2a0
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-06-14 21:46:11 +0700

    cool

--- modules/home/terminal/multiplexers/default.nix
+++ modules/home/terminal/multiplexers/default.nix
@@ -3,10 +3,24 @@
   lib,
   flake,
   ...
-}: {
-  imports = [./all.nix];
-  options.${flake.config.me.namespace}.terminal.multiplexers = {
-    tmux.enable = lib.mkEnableOption "tmux";
-    zellij.enable = lib.mkEnableOption "zellij";
-  };
+}: let inherit (flake.config.me) namespace; in {
+  options.${namespace}.terminal.tools.tmux = lib.mkEnableOption "tmux";
+  options.${namespace}.terminal.tools.zellij = lib.mkEnableOption "zellij";
+  config = lib.mkMerge [
+    (lib.mkIf config.${namespace}.terminal.tools.tmux {
+      programs.tmux = {
+        enable = true;
+        mouse = true;
+        prefix = "C-a";
+        tmuxinator.enable = true;
+        keyMode = "vi";
+      };
+    })
+    (lib.mkIf config.${namespace}.terminal.tools.zellij {
+      programs.zellij = {
+        enable = true;
+        settings.show_startup_tips = false;
+      };
+    })
+  ];
 }
